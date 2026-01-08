43b5ec449088504974dc64834014eaa77ca8949c
Shiva Dwivedi <dwivedishiva2006@gmail.com>
2026-01-08 23:00:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
-from dotenv import load_dotenv
+import streamlit as st
 
-load_dotenv()
+def get_google_api_key():
+    # Streamlit Cloud
+    if "GOOGLE_API_KEY" in st.secrets:
+        return st.secrets["GOOGLE_API_KEY"]
 
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+    # Local development (.env)
+    return os.getenv("GOOGLE_API_KEY")
